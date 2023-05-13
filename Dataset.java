d3288d0381b016254f766ed79878be0dc94e1c3c
Rahan Hasan <Rahanswayy2@gmail.com>
2023-05-13 16:43:05 -0400

    "patient and dataset class"

---
+++ Dataset.java
@@ -0,0 +1,112 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dataset {
+    private Patient[] patients;
+    private int totalNumber;
+    private int datasetID;
+
+    public Dataset(Patient[] patients, int totalNumber, int datasetID) {
+        this.patients = patients;
+        this.totalNumber = totalNumber;
+        this.datasetID = datasetID;
+    }
+
+    // Getter methods for All Attributes
+    public Patient[] getPatients() {
+        return patients;
+    }
+
+    public int getTotalNumber() {
+        return totalNumber;
+    }
+
+    public int getDatasetID() {
+        return datasetID;
+    }
+
+    // Setter methods for All Attributes
+    public void setPatients(Patient[] patients) {
+        this.patients = patients;
+    }
+
+    public void setTotalNumber(int totalNumber) {
+        this.totalNumber = totalNumber;
+    }
+
+    public void setDatasetID(int datasetID) {
+        this.datasetID = datasetID;
+    }
+
+    //Method to create new Dataset
+    public Dataset NewDataSet(Patient[] patients, int totalNumber, int datasetID) {
+        Dataset newDataset =  new Dataset(patients, totalNumber, datasetID);
+        return newDataset;
+    }
+
+    //Method to read csv file
+    public Dataset readFile(File file) throws IOException {
+        String fileName = file.getName();
+        String extension = fileName.substring(fileName.lastIndexOf(".") + 1);
+        //checking file extension
+        if (!extension.equals("csv")) {
+            throw new IOException("File format not supported.It must be CSV file");
+        }
+
+        //Reading data from file
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        List<Patient> patientList = new ArrayList<>();
+        String line = reader.readLine();
+        while ((line = reader.readLine()) != null) {
+            String[] patientData = line.split(",");
+            int patientID = Integer.parseInt(patientData[0].trim());
+            int age = Integer.parseInt(patientData[1].trim());
+            String gender = patientData[2].trim();
+            double bmi = Double.parseDouble(patientData[3].trim());
+            String bloodPressure = patientData[4].trim();
+            double totalSerumControl = Double.parseDouble(patientData[5].trim());
+            double ldl = Double.parseDouble(patientData[6].trim());
+            double hdl = Double.parseDouble(patientData[7].trim());
+            double tch = Double.parseDouble(patientData[8].trim());
+            double ltg = Double.parseDouble(patientData[9].trim());
+            double glu = Double.parseDouble(patientData[10].trim());
+            double diseaseProgressionValue = Double.parseDouble(patientData[11].trim());
+
+            Patient patient = new Patient(age, gender, bmi, bloodPressure, totalSerumControl, ldl, hdl, tch, ltg, glu, diseaseProgressionValue);
+            patientList.add(patient);
+        }
+        reader.close();
+
+        //creating dataset
+        Patient[] patients = new Patient[patientList.size()];
+        patients = patientList.toArray(patients);
+        int totalNumber = patients.length;
+        int datasetID = (int) (Math.random());
+
+        return new Dataset(patients, totalNumber, datasetID);
+    }
+
+    //Method to validate patient data
+    public Patient validatePatientData(Patient patient) throws Exception {
+        // Check for null or empty string values
+        if (patient.getGender() == null || patient.getGender().isEmpty() ||
+                patient.getBloodPressure() == null || patient.getBloodPressure().isEmpty()) {
+            throw new Exception("Invalid patient data: gender or BP are null or empty");
+        }
+
+        // Check age, BMI, and other integer/double fields
+        if (patient.getAge() <= 0 || patient.getBmi() <= 0 ||
+                patient.getTotalSerumControl() <= 0 || patient.getLdl() <= 0 ||
+                patient.getHdl() <= 0 || patient.getTch() <= 0 ||
+                patient.getLtg() <= 0 || patient.getGlu() <= 0) {
+            throw new Exception("Invalid patient data: age, BMI, or lab values are negative or 0");
+        }
+        // if All checks passed, patient data is valid
+        return patient;
+    }
+
+}
--- Patient.java
+++ Patient.java
@@ -1,44 +1,134 @@
 
 public class Patient {
-    int patientID;
-    int age;
-    int gender; //0 male; 1 female
-    double bmi;
-    double bloodPressure;
-    double totalSerumCholesterol;
-    double LDL, HDL, TCH, LTG,GLU;
-    double diseaseProgressionValue;
-    Patient next;
+    private int patientID;
+    private int age;
+    private String gender;
+    private double bmi;
+    private String bloodPressure;
+    private double totalSerumControl;
+    private double ldl;
+    private double hdl;
+    private double tch;
+    private double ltg;
+    private double glu;
+    private double diseaseProgressionValue;
 
-    public Patient(int ID, int a, int g, double b, double bp, double tsc, double lDL, double hDL, double tCH, double lTG, double gLU, double diseaseProgressionValue) {
-        patientID = ID;
-        age = a;
-        gender = g;
-        bmi = b;
-        bloodPressure = bp;
-        totalSerumCholesterol = tsc;
-        LDL = lDL;
-        HDL = hDL;
-        TCH = tCH;
-        LTG = lTG;
-        GLU = gLU;
+    public Patient(int age, String gender, double bmi, String bloodPressure, double totalSerumControl, double ldl, double hdl, double tch, double ltg, double glu, double diseaseProgressionValue) {
+        this.age = age;
+        this.gender = gender;
+        this.bmi = bmi;
+        this.bloodPressure = bloodPressure;
+        this.totalSerumControl = totalSerumControl;
+        this.ldl = ldl;
+        this.hdl = hdl;
+        this.tch = tch;
+        this.ltg = ltg;
+        this.glu = glu;
         this.diseaseProgressionValue = diseaseProgressionValue;
-        this.next = null;
     }
 
-    public Patient() {//dummy
-        patientID = -1;
-        age = 0;
-        gender = -1;
-        bmi = 0;
-        bloodPressure = 0;
-        totalSerumCholesterol = 0;
-        LDL = 0;
-        HDL = 0;
-        TCH = 0;
-        LTG =0;
-        GLU = 0;
-        diseaseProgressionValue =0;
-        next = null;
+    //gettter Method for All Attributes
+    public int getPatientID() {
+        return patientID;
     }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public double getBmi() {
+        return bmi;
+    }
+
+    public double getTotalSerumControl() {
+        return totalSerumControl;
+    }
+
+    public String getBloodPressure() {
+        return bloodPressure;
+    }
+
+    public double getLdl() {
+        return ldl;
+    }
+
+    public double getHdl() {
+        return hdl;
+    }
+
+    public double getTch() {
+        return tch;
+    }
+
+    public double getLtg() {
+        return ltg;
+    }
+
+    public double getGlu() {
+        return glu;
+    }
+
+    public double getDiseaseProgressionValue() {
+        return diseaseProgressionValue;
+    }
+
+    //Setter Methods for All Attributes
+    public void setLdl(double ldl) {
+        this.ldl = ldl;
+    }
+
+    public void setPatientID(int patientID) {
+        this.patientID = patientID;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setBmi(double bmi) {
+        this.bmi = bmi;
+    }
+
+    public void setBloodPressure(String bloodPressure) {
+        this.bloodPressure = bloodPressure;
+    }
+
+    public void setTotalSerumControl(double totalSerumControl) {
+        this.totalSerumControl = totalSerumControl;
+    }
+
+    public void setHdl(double hdl) {
+        this.hdl = hdl;
+    }
+
+    public void setTch(double tch) {
+        this.tch = tch;
+    }
+
+    public void setLtg(double ltg) {
+        this.ltg = ltg;
+    }
+
+    public void setGlu(double glu) {
+        this.glu = glu;
+    }
+
+    public void setDiseaseProgressionValue(double diseaseProgressionValue) {
+        this.diseaseProgressionValue = diseaseProgressionValue;
+    }
+
+    //Method to create new patient with given details
+    public Patient NewPatient(int age, String gender, double bmi, String bloodPressure, double totalSerumControl, double ldl, double hdl, double tch, double ltg, double glu, double diseaseProgressionValue) {
+        Patient newPatient = new Patient(age, gender, bmi, bloodPressure, totalSerumControl, ldl, hdl, tch, ltg, glu, diseaseProgressionValue);
+        return newPatient;
+    }
+
 }
